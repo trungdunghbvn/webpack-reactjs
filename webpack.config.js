89d49efa781a405d9029b8e46baad98ca8217801
Trung Dung <trungdunghbvn@gmail.com>
2018-09-03 10:17:10 +0700

    import font and scss

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,19 @@ var config = {
         test : /\.js$/,
         exclude :'/node_modules',
         loader : 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader"
+        }, {
+            loader: "css-loader"
+        }, {
+            loader: "sass-loader",
+            options: {
+                includePaths: ["src/assets/css", "src/assets/css"]
+            }
+        }]
       }
     ]
   },
